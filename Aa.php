12ba9022d57b1ffbb27addd8210e82de6408ed8d
shadowbot82 <42908347+shadowbot82@users.noreply.github.com>
2018-10-17 22:59:27 +0000

    Create Aa.php

---
+++ Aa.php
@@ -0,0 +1,22 @@
+<?php
+
+@system("clear");
+@system("clear");
+@system("clear");
+//warna
+$biru="\033[1;34m";
+$turkis="\033[1;36m";
+$ijo="\033[1;92m";
+$putih="\033[1;37m";
+$red="\033[1;31m";
+$kuning="\033[1;33m";
+$t = "\n";
+
+echo $turkis."Masukan ls : ";
+$ls = trim(fgets(STDIN));
+echo $turkis."Masukan aid : ";
+$aid = trim(fgets(STDIN));
+echo $turkis."Masukan jumlah : ";
+$jml = trim(fgets(STDIN));
+echo $turkis."Sleep : ";
+$wait = trim(fgets(STDIN));
