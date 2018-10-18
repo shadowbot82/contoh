c2c7bdbcfe67df5ad73c25911ed407729be6d8ba
shadowbot82 <42908347+shadowbot82@users.noreply.github.com>
2018-10-18 01:23:00 +0000

    Update Aa.php

--- Aa.php
+++ Aa.php
@@ -12,11 +12,42 @@ $red="\033[1;31m";
 $kuning="\033[1;33m";
 $t = "\n";
 
-echo $turkis."Masukan ls : ";
-$ls = trim(fgets(STDIN));
-echo $turkis."Masukan aid : ";
-$aid = trim(fgets(STDIN));
-echo $turkis."Masukan jumlah : ";
-$jml = trim(fgets(STDIN));
-echo $turkis."Sleep : ";
-$wait = trim(fgets(STDIN));
+include(readline($turkis."masukan config : "));
+//echo"$turkis bot start $putih \n";
+print "BOT NEWSAKU...".$t.$kuning."[>] ".$putih."sedang login eaa...".$t;
+sleep(2);
+// login
+$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, "https://ai.caping.co.id/v2/user/ccsp/detail"); 
+			curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+$head = array();
+					$head[] = "Cookie: u=$uid;n=$n";
+					$head[] = "User-Agent: Mozilla/5.0 (Linux; Android 6.0.1; Redmi 3S Build/MMB29M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/66.0.3359.126 Mobile Safari/537.36;CapingNews/4.1.4";
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $head);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $head);
+		$result = curl_exec($ch);
+		$js = json_decode($result,true);
+		$akunrp = $js['data']['Akun Rp'];
+		$akuncoin = $js['data']['Akun Coin'];
+		if ($js['message'] == 'OK') {
+		echo $turkis."[*] ".$putih."login: ".$ijo.$js['message'].$t;
+		sleep(1);
+		echo $turkis."[•] ".$putih."coin: ".$ijo.$coin.$putih." || money: ".$ijo."Rp.".$money.$t.$t;
+		}
+		else
+		{
+		echo $red."[!] login gagal || ".$js['message'].$t;
+		exit;
+		}
+sleep(1);
+
+
+
+
+
+
+
+
+?>
