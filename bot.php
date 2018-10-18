7a6c4573a05f446625c580d49e32cf5db21a602a
shadowbot82 <42908347+shadowbot82@users.noreply.github.com>
2018-10-18 10:28:45 +0000

    Update bot.php

--- bot.php
+++ bot.php
@@ -23,8 +23,9 @@ $ch = curl_init();
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 $head = array();
-					$head[] = "Cookie: ls=$ls;aid=$aid";
-					$head[] = "User-Agent: Mozilla/5.0 (Linux; Android 6.0.1; Redmi 3S Build/MMB29M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/66.0.3359.126 Mobile Safari/537.36";
+					$head[] = "Cookie: id=$userId";
+					$head[] = "User-Agent: Dalvik/2.1.0 (Linux; U; Android 6.0.1; Andromax A26C4H Build/MMB29M)";
+
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $head);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $head);
 		$result = curl_exec($ch);
@@ -51,7 +52,7 @@ $x = 0;
 while($x<20):
 $x++;
 flush();
-$news		=	news($ls,$aid);
+$news		=	news($userId);
 $news1 = json_decode($news,true);
 if ($news1['message'] == 'OK') {
 	    echo $ijo."    ==> SUKSESS";
@@ -69,7 +70,7 @@ $v=0;
 while($v<10):
 $v++;
 flush();
-$video		=	video($ls,$aid);
+$video		=	video($userId);
 $video1 = json_decode($video,true);
 if ($video1['message'] == 'OK') {
 	    echo $ijo."    ==> BERHASIL ";
@@ -90,8 +91,9 @@ $ch = curl_init();
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 $headr = array();
-					$headr[] = "Cookie: ls=$ls;aid=$aid";
-					$headr[] = "User-Agent: Mozilla/5.0 (Linux; Android 6.0.1; Redmi 3S Build/MMB29M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/66.0.3359.126 Mobile Safari/537.36";
+					$headr[] = "Cookie: id=$userId";
+					$headr[] = "User-Agent: Dalvik/2.1.0 (Linux; U; Android 6.0.1; Andromax A26C4H Build/MMB29M)";
+ 
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headr);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headr);
 		$result = curl_exec($ch);
@@ -102,29 +104,32 @@ $headr = array();
 	  =====| RESULT |=====".$t;
 echo $putih."[•] Akun Rp : ".$ijo.$akunrp1.$putih." | Akun Coin : Rp.".$ijo.$akuncoin1.$t;
 #######################################################
-function news($ls,$aid){ 
+function news($userId){ 
 		
 		$ch = curl_init();
 			curl_setopt($ch, CURLOPT_URL, "https://utv.news/api/news/read/redward "); 
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 				$headers = array();
-				$headers[] = "Cookie: ls=$ls;aid=$aid";
-				$headers[] = "User-Agent: Mozilla/5.0 (Linux; Android 6.0.1; Redmi 3S Build/MMB29M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/66.0.3359.126 Mobile Safari/537.36";
+				$headers[] = "Cookie: id=$userId";
+				$headers[] = "User-Agent: Dalvik/2.1.0 (Linux; U; Android 6.0.1; Andromax A26C4H Build/MMB29M)";
+
 		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 		$result = curl_exec($ch);
 	return $result;
 }
 
 
-function video($ls,$aid){
+function video($userId){
 		$ch = curl_init();
 			curl_setopt($ch, CURLOPT_URL, "https://utv.news/api/video/read/rewardtime "); 
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 			$headers = array();
-					$headers[] = "Cookie: ls=$ls;aid=$aid";
-					$headers[] = "User-Agent: Mozilla/5.0 (Linux; Android 6.0.1; Redmi 3S Build/MMB29M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/66.0.3359.126 Mobile Safari/537.36";
+					$headers[] = "Cookie: id=$userId";
+					$headers[] = "User-Agent:Dalvik/2.1.0 (Linux; U; Android 6.0.1; Andromax A26C4H Build/MMB29M)
+ ";
+
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 		$result = curl_exec($ch);
